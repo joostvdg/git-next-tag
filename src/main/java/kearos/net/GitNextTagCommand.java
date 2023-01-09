36bf14ee829aee5bc7ff3be1008f40d608974f2e
Joost van der Griendt <joostvdg@gmail.com>
2023-01-09 22:36:30 +0000

    Initial commit

---
+++ src/main/java/kearos/net/GitNextTagCommand.java
@@ -0,0 +1,28 @@
+package kearos.net;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "git-next-tag", description = "...",
+        mixinStandardHelpOptions = true)
+public class GitNextTagCommand implements Runnable {
+
+    @Option(names = {"-v", "--verbose"}, description = "...")
+    boolean verbose;
+
+    public static void main(String[] args) throws Exception {
+        PicocliRunner.run(GitNextTagCommand.class, args);
+    }
+
+    public void run() {
+        // business logic here
+        if (verbose) {
+            System.out.println("Hi!");
+        }
+    }
+}
